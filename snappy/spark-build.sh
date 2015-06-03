3245a6de9d1e84d197a4903e14845575df086827
Sumedh Wale <sumwale@yahoo.com>
2015-06-03 14:51:00 +0530

    Changing scala version default back to 2.10

--- snappy/spark-build.sh
+++ snappy/spark-build.sh
@@ -4,17 +4,16 @@ MAVEN_OPTS="-Xmx2g -XX:MaxPermSize=512M -XX:ReservedCodeCacheSize=512m"
 export MAVEN_OPTS
 
 BUILDER="build/sbt"
-SCALA="scala-2.11"
+SCALA="scala-2.10"
 ARGS=
 if [ -n "$1" ]; then
-  EXPLICIT_SCALA=
   for arg in "$@"; do
     case "${arg}" in
       -scala-2.10)
-        EXPLICIT_SCALA="scala-2.10"
+        SCALA="scala-2.10"
       ;;
       -scala-2.11)
-        EXPLICIT_SCALA="scala-2.11"
+        SCALA="scala-2.11"
       ;;
       -mvn)
         BUILDER=mvn
@@ -25,19 +24,16 @@ if [ -n "$1" ]; then
       ;;
     esac
   done
-  if [ -n "${EXPLICIT_SCALA}" ]; then
-    SCALA="${EXPLICIT_SCALA}"
-  fi
 fi
 
-if [ "${SCALA}" = "scala-2.11" ]; then
-  ./dev/change-version-to-2.11.sh
-elif [ "${SCALA}" = "scala-2.10" ]; then
+if [ "${SCALA}" = "scala-2.10" ]; then
   ./dev/change-version-to-2.10.sh
+elif [ "${SCALA}" = "scala-2.11" ]; then
+  ./dev/change-version-to-2.11.sh
 fi
 
 if [ -z "${ARGS}" ]; then
-  ${BUILDER} -Pyarn -Phadoop-2.4 -Dhadoop.version=2.4.1 -Phive -Phive-thriftserver -DskipTests -D${SCALA}
+  ${BUILDER} -Phadoop-2.4 -Dhadoop.version=2.4.1 -Phive -Phive-thriftserver -DskipTests -D${SCALA}
 else
-  eval ${BUILDER} -Pyarn -Phadoop-2.4 -Dhadoop.version=2.4.1 -Phive -Phive-thriftserver -DskipTests -D${SCALA} ${ARGS}
+  eval ${BUILDER} -Phadoop-2.4 -Dhadoop.version=2.4.1 -Phive -Phive-thriftserver -DskipTests -D${SCALA} ${ARGS}
 fi
