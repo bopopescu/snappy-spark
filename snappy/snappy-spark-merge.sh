5803380f9d0d2aaa29077dd9d6de4b435af15faa
Sumedh Wale <swale@pivotal.io>
2015-09-14 04:22:23 +0530

    updated upstream merge script

--- snappy/snappy-spark-merge.sh
+++ snappy/snappy-spark-merge.sh
@@ -1,3 +1,7 @@
-git fetch upstream
+#!/bin/sh
+
+git fetch origin -v
+git fetch upstream -v
 git checkout snappy/master
-git merge upstream/master
+git rebase origin/snappy/master
+git merge upstream/master "$@"
